83f85fa6234cc2ec8fafa1afc87bf4451e2a04f8
diego890 <frankdiego1993@gmail.com>
2016-06-27 09:34:27 -0500

    Update mensajes.php

--- mensajes.php
+++ mensajes.php
@@ -1,5 +1,5 @@
 <?php 
-	$con = mysqli_connect("localhost","root","","chat");
+	$con = mysqli_connect("us-cdbr-azure-southcentral-f.cloudapp.net","b756bee20634b8","93d3da43","chat3");
 	$sql = "SELECT * FROM mensajes ORDER BY timestamp DESC LIMIT 1";
 
 	echo $res = mysqli_query($con,$sql) or die(mysqli_error());
