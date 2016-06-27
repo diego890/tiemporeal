1146cac6199861deef3ff276873a66db52794f06
diego890 <frankdiego1993@gmail.com>
2016-06-27 12:22:42 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	mysql_connect("us-cdbr-azure-southcentral-f.cloudapp.net","bf11601bdd6e36","6bff6f7b");
-	mysql_select_db("acsm_b46a38908b8b7e4");
+	mysql_connect('us-cdbr-azure-southcentral-f.cloudapp.net','bf11601bdd6e36','6bff6f7b');
+	mysql_select_db('acsm_b46a38908b8b7e4');
 
 ?>
