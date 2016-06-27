0d8637293870f1ce11b0bdaf62346faad7eb519a
diego890 <frankdiego1993@gmail.com>
2016-06-26 19:05:36 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	mysql_connect("Localhost","root","");
-	mysql_select_db("chat");
+	mysql_connect("us-cdbr-azure-southcentral-f.cloudapp.net","b756bee20634b8","93d3da43");
+	mysql_select_db("chat3");
 
 ?>
