14323de39c6c93494e5560bde75b68c85db8f4b0
diego890 <frankdiego1993@gmail.com>
2016-06-27 22:20:39 -0500

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	$con =mysqli_connect('us-cdbr-azure-southcentral-f.cloudapp.net','b4c88883a80208','e4f28037','chat');
+	$con =mysqli_connect('us-cdbr-azure-southcentral-f.cloudapp.net','b4c88883a80208','e4f28037','acsm_56e566ee868c1f3');
 	mysqli_set_charset($con, 'utf8');
 	$mensaje = $_POST['mensaje'];
 	$timestamp = date("Y-m-d H:i:s");
